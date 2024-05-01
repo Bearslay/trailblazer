c76368ea8026d6805e853a710cf020055f1e5d9d
Bearslay <barfunkel@protonmail.com>
2024-05-01 06:48:55 -0400

    resolved linker errors with template functions

--- include/Utilities.hpp
+++ include/Utilities.hpp
@@ -85,13 +85,21 @@ namespace btils {
      * @param lhs The left-hand side of an assignment operator (value will be altered)
      * @param rhs The right-hand side of an assignment operator (value will not be altered)
      * @returns The original value of lhs    */
-    template <typename Type> Type set(Type &lhs, const Type &rhs);
+    template <typename Type> Type set(Type &lhs, const Type &rhs) {
+        const Type output = lhs;
+        lhs = rhs;
+        return output;
+    }
     /** Acts as an addition assignment operator, but while returning the previous value of the thing being modified
      * @tparam Type A something that has a defined addition assignment operator (primitive type or otherwise)
      * @param lhs The left-hand side of an addition assignment operator (value will be altered)
      * @param rhs The right-hand side of an addition assignment operator (value will not be altered)
      * @returns The original value of lhs    */
-    template <typename Type> Type adj(Type &lhs, const Type &rhs);
+    template <typename Type> Type adj(Type &lhs, const Type &rhs) {
+        const Type output = lhs;
+        lhs += rhs;
+        return output;
+    }
 
     /** Maps a value within a range onto a different range
      * @tparam InArithType An arithmetic data type for the input & starting range
@@ -102,7 +110,11 @@ namespace btils {
      * @param outMin Minimum value for the ending range (of OutArithType type)
      * @param outMax Maximum value for the ending range (of OutArithType type)
      * @returns A value within the ending range matching the input value's position within the starting range (of OutArithType type)    */
-    template <typename InArithType, typename OutArithType> OutArithType map(const InArithType &input, const InArithType &inMin, const InArithType &inMax, const OutArithType &outMin, const OutArithType &outMax);
+    template <typename InArithType, typename OutArithType> OutArithType map(const InArithType &input, const InArithType &inMin, const InArithType &inMax, const OutArithType &outMin, const OutArithType &outMax) {
+        static_assert(std::is_arithmetic< InArithType>::value, "InArithType must be an arithmetic type");
+        static_assert(std::is_arithmetic<OutArithType>::value, "OutArithType must be an arithmetic type");
+        return outMin + (OutArithType)(input - inMin) * (outMax - outMin) / (OutArithType)(inMax - inMin);
+    }
     /** Maps a value within a range onto a different range
      * @tparam ArithType An arithmetic data type for the input & starting range
      * @param input Value to be mapped
@@ -111,7 +123,7 @@ namespace btils {
      * @param outMin Minimum value for the ending range
      * @param outMax Maximum value for the ending range
      * @returns A value within the ending range matching the input value's position within the starting range    */
-    template <typename ArithType> ArithType map(const ArithType &input, const ArithType &inMin, const ArithType &inMax, const ArithType &outMin, const ArithType &outMax);
+    template <typename ArithType> ArithType map(const ArithType &input, const ArithType &inMin, const ArithType &inMax, const ArithType &outMin, const ArithType &outMax) {return map<ArithType, ArithType>(input, inMin, inMax, outMin, outMax);}
 
     /** Normalize a value to a range (assuming the range is a loop)
      * @tparam ArithType An arithmetic data type for the input & range
@@ -119,13 +131,21 @@ namespace btils {
      * @param rangeMin The smallest value making up the range (inclusive)
      * @param rangeMax The largest value making up the range (exclusive)
      * @returns A value normalized in the given range    */
-    template <typename ArithType> ArithType normalize(const ArithType &input, const ArithType &rangeMin, const ArithType &rangeMax);
+    template <typename ArithType> ArithType normalize(const ArithType &input, const ArithType &rangeMin, const ArithType &rangeMax) {
+        static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
+        const ArithType width = rangeMax - rangeMin;
+        const ArithType value = input - rangeMin;
+        return (value - (std::floor(value / width) * width)) + rangeMin;
+    }
     /** Normalize a value to a range with a minimum value of 0 (assuming the range is a loop)
      * @tparam ArithType An arithmetic data type for the input & range
      * @param input The value to normalize
      * @param max The largest value making up the range (exclusive)
      * @returns A value normalized in the given range    */
-    template <typename ArithType> ArithType normalize(const ArithType &input, const ArithType &max);
+    template <typename ArithType> ArithType normalize(const ArithType &input, const ArithType &max) {
+        static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
+        return input - std::floor(input / max) * max;
+    }
 
     /** Clamp a value to a range
      * @tparam ArithType An arithmetic data type for the input & range
@@ -133,13 +153,19 @@ namespace btils {
      * @param rangeMin The smallest value making up the range (inclusive)
      * @param rangeMax The largest value making up the range (inclusive)
      * @returns A value clammped to the given range    */
-    template <typename ArithType> ArithType clamp(const ArithType &input, const ArithType &rangeMin, const ArithType &rangeMax);
+    template <typename ArithType> ArithType clamp(const ArithType &input, const ArithType &rangeMin, const ArithType &rangeMax) {
+        static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
+        return input < rangeMin ? rangeMin : (input > rangeMax ? rangeMax : input);
+    }
     /** Clamp a value to a range with a minimum value of 0
      * @tparam ArithType An arithmetic data type for the input & range
      * @param input The value to clamp
      * @param max The largest value making up the range (inclusive)
      * @returns A value clammped to the given range    */
-    template <typename ArithType> ArithType clamp(const ArithType &input, const ArithType &max);
+    template <typename ArithType> ArithType clamp(const ArithType &input, const ArithType &max) {
+        static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
+        return input < 0 ? 0 : (input > max ? max : input);
+    }
 
     /** Convert an angle from degrees to radians
      * @param angle An angle in degrees
--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -1,43 +1,5 @@
 #include "Utilities.hpp"
 
-template <typename Type> Type btils::set(Type &lhs, const Type &rhs) {
-    const Type output = lhs;
-    lhs = rhs;
-    return output;
-}
-template <typename Type> Type btils::adj(Type &lhs, const Type &rhs) {
-    const Type output = lhs;
-    lhs += rhs;
-    return output;
-}
-
-template <typename InArithType, typename OutArithType> OutArithType btils::map(const InArithType &input, const InArithType &inMin, const InArithType &inMax, const OutArithType &outMin, const OutArithType &outMax) {
-    static_assert(std::is_arithmetic< InArithType>::value, "InArithType must be an arithmetic type");
-    static_assert(std::is_arithmetic<OutArithType>::value, "OutArithType must be an arithmetic type");
-    return outMin + ((OutArithType)input - (OutArithType)inMin) * (outMax - outMin) / ((OutArithType)inMax - (OutArithType)inMin);
-}
-template <typename ArithType> ArithType btils::map(const ArithType &input, const ArithType &inMin, const ArithType &inMax, const ArithType &outMin, const ArithType &outMax) {return map<ArithType, ArithType>(input, inMin, inMax, outMin, outMax);}
-
-template <typename ArithType> ArithType btils::normalize(const ArithType &input, const ArithType &rangeMin, const ArithType &rangeMax) {
-    static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
-    const ArithType width = rangeMax - rangeMin;
-    const ArithType value = input - rangeMin;
-    return (value - (std::floor(value / width) * width)) + rangeMin;
-}
-template <typename ArithType> ArithType btils::normalize(const ArithType &input, const ArithType &max) {
-    static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
-    return input - std::floor(input / max) * max;
-}
-
-template <typename ArithType> ArithType btils::clamp(const ArithType &input, const ArithType &rangeMin, const ArithType &rangeMax) {
-    static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
-    return input < rangeMin ? rangeMin : (input > rangeMax ? rangeMax : input);
-}
-template <typename ArithType> ArithType btils::clamp(const ArithType &input, const ArithType &max) {
-    static_assert(std::is_arithmetic<ArithType>::value, "ArithType must be an arithmetic type");
-    return input < 0 ? 0 : (input > max ? max : input);
-}
-
 double btils::degToRad(const double &angle) {return angle * U_PI_180;}
 double btils::radToDeg(const double &angle) {return angle * U_180_PI;}
 double btils::convertAngle(const double &angle, const bool &toRadians) {return toRadians ? degToRad(angle) : radToDeg(angle);}
